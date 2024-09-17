91bd3b2891f55528667803b8b834e88a90fced89
Kajtek Rut <kajetan.rut.mail@gmail.com>
2024-09-17 14:24:35 +0200

    aa

---
+++ src/main/java/org/technischools/obiektowkaproj/ObiektowkaprojApplication.java
@@ -0,0 +1,13 @@
+package org.technischools.obiektowkaproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObiektowkaprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObiektowkaprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/technischools/obiektowkaproj/ObiektowkaprojApplicationTests.java
@@ -0,0 +1,13 @@
+package org.technischools.obiektowkaproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObiektowkaprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
